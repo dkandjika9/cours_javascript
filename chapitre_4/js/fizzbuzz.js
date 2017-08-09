0f5799cc6c89d8a1d998810925c7c000f3f18f07
Kandjika <dadjojoel@gmail.com>
2017-08-09 10:50:09 +0000

    Modification version1

--- chapitre_4/js/fizzbuzz.js
+++ chapitre_4/js/fizzbuzz.js
@@ -1,11 +1,16 @@
 for (var i = 1; i <=100; i++) {
-	console.log(i);
 	var divisibleTrois = "fizz";
 	var divisibleCinq = "buzz";
-	if (i % 3 === 0) {
+	var divisibleCinqTrois = "fizzbuzz";
+	if ((i % 3 === 0) && (i % 5 === 0)) {
+       console.log(divisibleCinqTrois);
+	}
+	else if(i % 3 === 0) {
      console.log(divisibleTrois);
 	} else if (i % 5 === 0) {
      console.log(divisibleCinq);
-	} 
+	} else {
+		console.log(i);
+	}
 	
 }
